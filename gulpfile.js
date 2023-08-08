c5c534f14971693fd3fbc7838cd93418a638e69a
AndreySchmidt <ringnull@gmail.com>
2023-08-08 19:31:30 +0300

    gulp part 2

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,18 @@ const sourceMaps = require("gulp-sourcemaps");
 // const groupMedia = require("gulp-group-css-media-queries");
 const plumber = require("gulp-plumber");
 const notify = require("gulp-notify");
+const webpack = require("webpack-stream");
+const babel = require("gulp-babel");
+
+const plumberConfig = (title) => {
+  return {
+    errorHandler: notify.onError({
+      title: title,
+      message: "Error <%= error.message %>",
+      sound: false,
+    }),
+  };
+};
 
 gulp.task("clean", function (done) {
   if (fs.existsSync(".dist/")) {
@@ -17,53 +29,23 @@ gulp.task("clean", function (done) {
   done();
 });
 
-//to run in console:  gulp Taskname
-// gulp.task("Taskname", function (done) {
-//   console.log("Hello");
-
-//   done();
-// });
-
-// console: gulp
-// gulp.task("default", gulp.series("Taskname"));
-
 const fileIncludeSettings = {
   prefix: "@@",
   basepath: "@file",
 };
 
-
-const plumberHtmlConfig = {
-  errorHandler: notify.onError({
-    title: "Html",
-    message: "Error <%= error.message %>",
-    sound: false,
-  }),
-};
-
-
-//to run in console:  gulp fileListInclude
-// gulp.task("fileListInclude", function () {
 gulp.task("html", function () {
   return gulp
     .src("./src/*.html")
-    .pipe(plumber(plumberHtmlConfig))
+    .pipe(plumber(plumberConfig("Html")))
     .pipe(fileInclude(fileIncludeSettings).pipe(gulp.dest("./dist/")));
 });
-
-const plumberSassConfig = {
-  errorHandler: notify.onError({
-    title: "Styles",
-    message: "Error <%= error.message %>",
-    sound: false,
-  }),
-};
 
 gulp.task("sass", function () {
   return (
     gulp
       .src("./src/scss/*.scss")
-      .pipe(plumber(plumberSassConfig))
+      .pipe(plumber(plumberConfig("Styles")))
       .pipe(sourceMaps.init())
       .pipe(sass())
       // .pipe(groupMedia())
@@ -72,9 +54,25 @@ gulp.task("sass", function () {
   );
 });
 
-// gulp.task("copyImg", function () {
 gulp.task("img", function () {
   return gulp.src("./src/img/**/*").pipe(gulp.dest("./dist/img/"));
+});
+
+gulp.task("fonts", function () {
+  return gulp.src("./src/fonts/**/*").pipe(gulp.dest("./dist/fonts/"));
+});
+
+gulp.task("files", function () {
+  return gulp.src("./src/files/**/*").pipe(gulp.dest("./dist/files/"));
+});
+
+gulp.task("js", function () {
+  return gulp
+    .src("./src/js/*.js")
+    .pipe(plumber(plumberConfig("Js")))
+    .pipe(babel())
+    .pipe(webpack(require("./webpack.config.js")))
+    .pipe(gulp.dest("./dist/js/"));
 });
 
 const serverOptions = {
@@ -82,7 +80,6 @@ const serverOptions = {
   open: true,
 };
 
-// gulp.task("startServer", function () {
 gulp.task("server", function () {
   return gulp.src("./dist/").pipe(server(serverOptions));
 });
@@ -91,13 +88,16 @@ gulp.task("watch", function () {
   gulp.watch("./src/scss/**/*.scss", gulp.parallel("sass"));
   gulp.watch("./src/**/*.html", gulp.parallel("html"));
   gulp.watch("./src/img/**/*", gulp.parallel("img"));
+  gulp.watch("./src/fonts/**/*", gulp.parallel("fonts"));
+  gulp.watch("./src/files/**/*", gulp.parallel("files"));
+  gulp.watch("./src/js/**/*.js", gulp.parallel("js"));
 });
 
 gulp.task(
   "default",
   gulp.series(
     "clean",
-    gulp.parallel("html", "sass", "img"),
+    gulp.parallel("html", "sass", "img", "fonts", "files", "js"),
     gulp.parallel("server", "watch")
   )
 );
--- readme.txt
+++ readme.txt
@@ -1,3 +1,6 @@
 npm i gulp gulp-sass sass gulp-file-include gulp-clean gulp-server-livereload gulp-sourcemaps gulp-plumber gulp-notify gulp-group-css-media-queries --save-dev
 
 sudo npm i --global gulp-cli
+
+npm i webpack-stream style-loader css-loader --save-dev
+npm i gulp-babel @babel/core @babel/preset-env --save-dev
